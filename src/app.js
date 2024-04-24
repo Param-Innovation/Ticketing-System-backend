50397eb9a6e6dd0ace462cb5970baec92e745621
Software <software@Params-iMac.local>
2024-04-24 18:36:37 +0530

    Implmented Slots related API

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./Database/dbConnect');
 const userRoutes = require('./routes/userRoutes');
+const slotRoutes = require('./routes/slotRoutes');
 
 const app = express();
 app.use(express.json()); // Middleware to parse JSON request bodies
@@ -8,6 +9,9 @@ app.use(express.urlencoded({ extended: true }));  // For parsing application/x-w
 
 connectDB();
 
+// Mount user routes at /api/users
 app.use('/api/users', userRoutes);
+// Mount slot routes at /api/slots
+app.use('/api/slots', slotRoutes);
 
 module.exports = app;
---
+++ src/routes/slotRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const slotController = require('../controller/slotController');
+
+router.get('/:date', slotController.getOrCreateSlots);
+
+module.exports = router;
